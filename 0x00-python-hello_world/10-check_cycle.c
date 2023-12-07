7e20ef44f9303d6210bf9c4a6acc1800d519658f
islam <islam003007@gmail.com>
2023-12-07 11:25:37 +0200

    new

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the linked list has a cycle.
+ *
+ * @list: inputs head.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	while (list != NULL)
+	{
+		list = list->next;
+		if (list == temp)
+			return (1);
+	}
+
+	return (0);
+}
