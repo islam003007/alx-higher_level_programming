38847a30c35d56a3096e7851319c4462b32227e4
islam <islam003007@gmail.com>
2023-12-18 16:44:00 +0200

    new

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(argc - 1))
+        for i in range(argc - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        
