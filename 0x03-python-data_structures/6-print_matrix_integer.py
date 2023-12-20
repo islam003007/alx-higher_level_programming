60679aaba1f845863b8a5ea7096973549d0e258e
islam <islam003007@gmail.com>
2023-12-20 21:49:52 +0200

    new

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        if not i:
+            continue
+        print("{:d}".format(i[0]), end="")
+        for j in i[1:]:
+            print(" {:d}".format(j), end="")
+        print("")
