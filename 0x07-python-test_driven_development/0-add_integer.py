1cd0fa17c956f9707c022838411ac3c48f90f15f
islam <islam003007@gmail.com>
2024-01-27 14:19:04 +0200

    new

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """"adds 2 intgers"""
 
+
 def add_integer(a, b=98):
     """adds 2 intgers"""
-    
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     if isinstance(a, float):
         a = int(a)
-    
+
     if isinstance(b, float):
         b = int(b)
-    
+
     return a + b
 
-    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
