c92d8d6f6909dbe58343a981495d001324d18870
islam <islam003007@gmail.com>
2024-01-27 13:04:53 +0200

    new

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""adds 2 intgers"""
+
+def add_integer(a, b=98):
+    """adds 2 intgers"""
+    
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    if isinstance(a, float):
+        a = int(a)
+    
+    if isinstance(b, float):
+        b = int(b)
+    
+    return a + b
+
+    
