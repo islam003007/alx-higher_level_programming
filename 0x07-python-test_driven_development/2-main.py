76415013cd1fa1a79a43b1da1dfb30b460f966b0
islam <islam003007@gmail.com>
2024-01-29 12:58:27 +0200

    new

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -5,6 +5,6 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
-print(matrix_divided(matrix, float("NaN")))
+print(matrix_divided())
 print(matrix)
 
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,29 +4,30 @@
 
 def matrix_divided(matrix, div):
     """devides all elements of a function"""
-    
+
+    type_error = "matrix must be a matrix (list of lists) of integers/floats"
     row_length = len(matrix[0])
-    
+
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+        raise TypeError(type_error)
+
     for i in matrix:
         if type(i) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(type_error)
         if len(i) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
             if type(j) not in (int, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                raise TypeError(type_error)
+
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = []
-    
+
     for i in matrix:
         new_row = []
         for j in i:
@@ -34,7 +35,7 @@ def matrix_divided(matrix, div):
         new_matrix.append(new_row)
 
     return new_matrix
-   
+
 
 if __name__ == "__main__":
     import doctest
