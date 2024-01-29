9c36ee35390926e4202a5cb5afa5c9c41f5aa3ff
islam <islam003007@gmail.com>
2024-01-29 11:38:10 +0200

    new

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""divides all elements of a fucntion"""
+
+
+def matrix_divided(matrix, div):
+    """devides all elements of a function"""
+    
+    row_length = len(matrix[0])
+    
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(i) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in i:
+            if type(j) not in (int, float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+    
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    new_matrix = []
+    
+    for i in matrix:
+        new_row = []
+        for j in i:
+            new_row.append(round(j/div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
+   
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
