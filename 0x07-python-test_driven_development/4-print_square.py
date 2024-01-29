ef88857637a5589f469f362b1b57cdf351b3a0f8
islam <islam003007@gmail.com>
2024-01-29 14:05:22 +0200

    new

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,13 +4,13 @@
 
 def print_square(size):
     """prints a square with the character #"""
-    
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for i in range(size):
         print(size * "#")
 
