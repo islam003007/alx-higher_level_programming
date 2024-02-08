3ae5dac6fcecdafe4edb59639de34f3be0cfbcaa
islam <islam003007@gmail.com>
2024-02-08 10:59:42 +0200

    new

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,15 +4,15 @@
 
 class Rectangle:
     """Rectangle  Class"""
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, width):
         if type(width) is not int:
@@ -20,11 +20,11 @@ class Rectangle:
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, height):
         if type(height) is not int:
@@ -32,13 +32,11 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-    
+
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         return (self.width + self.height) * 2
-    
-        
