889bb39ee6e5c2c5a2500fb2dec27c8396c5d1c0
islam <islam003007@gmail.com>
2024-03-07 13:52:41 +0200

    new

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,7 @@
 
 def is_same_class(obj, a_class):
     """isinstance() clone"""
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
