f3d184e77bbfafd12151581c8143e29e723437df
islam <islam003007@gmail.com>
2024-03-07 14:33:06 +0200

    new

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module has an improved BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class improved"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")
