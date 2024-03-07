bb030be9343887347020348730988b2e3c31e14b
islam <islam003007@gmail.com>
2024-03-07 13:48:27 +0200

    new

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this module has function that cis like isinstance()"""
+
+
+def is_same_class(obj, a_class):
+    """isinstance() clone"""
+    return isinstance(obj, a_class)
