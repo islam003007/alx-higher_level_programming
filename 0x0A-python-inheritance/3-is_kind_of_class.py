b58a1c447664ef5e6c63d04145522047ecdb8f3b
islam <islam003007@gmail.com>
2024-03-07 14:06:40 +0200

    new

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module has function\
+that checks for instance of class or parent class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks of instance of class or parent class"""
+    return isinstance(obj, a_class)
