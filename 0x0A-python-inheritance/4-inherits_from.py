738c5a6df8a3113b78862964669b19d4f5f24505
islam <islam003007@gmail.com>
2024-03-07 14:12:58 +0200

    new

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,12 +5,3 @@
 def inherits_from(obj, a_class):
     """checks for inheritance"""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
